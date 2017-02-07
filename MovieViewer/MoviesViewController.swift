ecba4b788ca7d2ca1b6daf2bf70cad4e2d54f4c8
gerardrecinto <grecinto@ucsd.edu>
2017-02-06 16:06:27 -0800

    commit help

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -14,9 +14,19 @@ import MBProgressHUD
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var search: UISearchBar!
+    let searchController = UISearchController(searchResultsController: nil)
+
     var movies: [NSDictionary]?
+    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+                "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+                "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+                "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+                "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
     
+    var filteredData: [String]!
+
     // Makes a network request to get updated data
     // Updates the tableView with the new data
     // Hides the RefreshControl
@@ -61,6 +71,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                                                                         MBProgressHUD.hide(for: self.view, animated: true)
                                                                         
                                                                         // ... Remainder of response handling code ...
+                                                                        if (error != nil){
+                                                                            self.errorLabel.isHidden = false
+                                                                        } else {
+                                                                            self.errorLabel.isHidden = true
+                                                                        }
+                                                                        
+                                                                        
                                                                         
         });
         task.resume()
@@ -69,8 +86,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchController.searchResultsUpdater = self.search as! UISearchResultsUpdating?
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.delegate = self.search as! UISearchControllerDelegate?
         tableView.dataSource = self
         tableView.delegate = self
+        filteredData = data
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         // add refresh control to table view
@@ -82,8 +105,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+       // if (request.hashValue == 0) {
+        
+        //}
+        
+
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-  
+        
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
 
             if let data = data {
@@ -109,6 +137,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         } else {
             return 0
         }
+        return filteredData.count
+
+    }
+    // This method updates filteredData based on the text in the Search Box
+    func search(_ search: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredData = searchText.isEmpty ? data : data.filter({(dataString: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return dataString.range(of: searchText, options: .caseInsensitive) != nil
+        })
+        
+        tableView.reloadData()
     }
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
@@ -121,7 +165,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         cell.posterView.setImageWith(imageUrl!)
-        
+        cell.textLabel?.text = filteredData[indexPath.row]
+
         return cell
     }
 
