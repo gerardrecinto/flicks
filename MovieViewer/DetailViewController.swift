b2e760e0d9f7f825c207e21bd09ce91914cfdb81
gerardrecinto <grecinto@ucsd.edu>
2017-02-13 00:55:44 -0800

    week2 plus 2 optionals

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -33,8 +33,54 @@ class DetailViewController: UIViewController {
 
         if let posterPath = movie["poster_path"] as? String{
             
-            let imageUrl = URL(string: baseUrl + posterPath)
-            posterImageView.setImageWith(imageUrl!)
+            
+            let smallImageUrl = URL(string: "https://image.tmdb.org/t/p/w45" + posterPath)
+            let largeImageUrl = URL(string: "https://image.tmdb.org/t/p/original" + posterPath)
+            let smallImageRequest = NSURLRequest(url: smallImageUrl!)
+            let largeImageRequest = NSURLRequest(url: largeImageUrl!)
+            
+            self.posterImageView.setImageWith(
+                smallImageRequest as URLRequest,
+                placeholderImage: nil,
+                success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                    
+                    // smallImageResponse will be nil if the smallImage is already available
+                    // in cache (might want to do something smarter in that case).
+                    self.posterImageView.alpha = 0.0
+                    self.posterImageView.image = smallImage;
+                    
+                    UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                        
+                        self.posterImageView.alpha = 1.0
+                        
+                    }, completion: { (sucess) -> Void in
+                        
+                        // The AFNetworking ImageView Category only allows one request to be sent at a time
+                        // per ImageView. This code must be in the completion block.
+                        self.posterImageView.setImageWith(
+                            largeImageRequest as URLRequest,
+                            placeholderImage: smallImage,
+                            success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                                
+                                self.posterImageView.image = largeImage;
+                                
+                        },
+                            failure: { (request, response, error) -> Void in
+                                // do something for the failure condition of the large image request
+                                // possibly setting the ImageView's image to a default image
+                        })
+                    })
+            },
+                failure: { (request, response, error) -> Void in
+                    // do something for the failure condition
+                    // possibly try to get the large image
+            })
+
+            //let imageUrl = URL(string: baseUrl + posterPath)
+            //posterImageView.setImageWith(imageUrl!)
+            
+            
+            
         }
         
         print(movie)
